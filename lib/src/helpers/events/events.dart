a4dc1091e642a3d26b2d1d65f48dfb7d0a224ef4
Kevin Moore <kevmoo@google.com>
2023-11-22 10:34:46 -0800

    Add event extensions for `WebSocket`
    Cleanup SDK constraint now that Dart 3.2 is stable

--- lib/src/helpers/events/events.dart
+++ lib/src/helpers/events/events.dart
@@ -289,3 +289,13 @@ extension WindowCustomEvents on Window {
   Stream<TransitionEvent> get onTransitionEnd =>
       CustomEventProviders.transitionEndEvent.forTarget(this);
 }
+
+extension WebSocketEvents on WebSocket {
+  Stream<Event> get onOpen => EventStreamProviders.openEvent.forTarget(this);
+  Stream<MessageEvent> get onMessage =>
+      EventStreamProviders.messageEvent.forTarget(this);
+  Stream<CloseEvent> get onClose =>
+      EventStreamProviders.closeEvent.forTarget(this);
+  Stream<Event> get onError =>
+      EventStreamProviders.errorEventSourceEvent.forTarget(this);
+}

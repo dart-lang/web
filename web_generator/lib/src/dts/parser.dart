de6b3e40826bde1653573d5c8e36aec3df2f4927
Srujan Gaddam <58529443+srujzs@users.noreply.github.com>
2025-06-06 21:40:50 -0700

    Add missing copyrights and delete empty files (#371)

--- web_generator/lib/src/dts/parser.dart
+++ web_generator/lib/src/dts/parser.dart
@@ -1,2 +1,6 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // TODO(nikeokoronkwo): Implement this function
 dynamic parseDeclarationFiles(Iterable<String> files) {}

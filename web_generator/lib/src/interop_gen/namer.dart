0512cde1786c4db5fbbd847d514ce0a298ec18a6
Nike Okoronkwo <nikechukwu@gmail.com>
2025-06-18 00:35:19 -0400

    added header comments, and some changes

--- web_generator/lib/src/interop_gen/namer.dart
+++ web_generator/lib/src/interop_gen/namer.dart
@@ -1,4 +1,6 @@
-// typedef ID = ({String type, String name, String? index});
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 class ID {
   final String type;
--- web_generator/lib/src/interop_gen/transform.dart
+++ web_generator/lib/src/interop_gen/transform.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:js_interop';
 
 import 'package:code_builder/code_builder.dart';
@@ -34,7 +38,7 @@ class TransformResult {
   }
 }
 
-/// A map of declarations, where the key is the declaration's ID.
+/// A map of declarations, where the key is the declaration's stringified [ID].
 extension type NodeMap._(Map<String, Node> decls) implements Map<String, Node> {
   NodeMap() : decls = <String, Node>{};
 

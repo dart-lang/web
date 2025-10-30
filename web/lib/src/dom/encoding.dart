d089a69aa6c7d05c875ff8615f7b082bcdcb53da
Srujan Gaddam <58529443+srujzs@users.noreply.github.com>
2025-10-30 15:06:25 -0700

    Modify lint ignores in package:web (#481)
    Fixes https://github.com/dart-lang/web/issues/479
    
    Now that we have a general purpose IDL->interop generator, we need
    to add ignores for constant_identifier_names and non_constant_identifier_names
    in every output file. unintended_html_in_doc_comment no longer applies
    as the original reason for it has been fixed.
    
    Removes the two lints from analysis_options.yaml as it would now be
    redundant.
    
    Also fixes some issues around experimental_member_use by updating the
    analyzer and ignoring it for redeclares.

--- web/lib/src/dom/encoding.dart
+++ web/lib/src/dom/encoding.dart
@@ -8,7 +8,7 @@
 
 // Generated from Web IDL definitions.
 
-// ignore_for_file: unintended_html_in_doc_comment
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
 @JS()
 library;

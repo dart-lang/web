92338e7c2089836d01befe118101adb900632d0f
Natalie Weizenbaum <nweiz@google.com>
2025-12-10 15:10:58 -0800

    Document js_linterop/lib/unsafe.dart

--- js_interop/lib/unsafe.dart
+++ js_interop/lib/unsafe.dart
@@ -2,4 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// Like `dart:js_interop_unsafe`, this library contains utilities that treat JS
+/// objects as arbitrary sets of properties as well as those that expose JS's
+/// runtime reflection capabilities. It should be used with care as it can
+/// invalidate assumptions made by the statically type-annotated JS APIs used
+/// elsewhere.
+library;
+
 export 'src/unsafe/object.dart';

30d7a94fe3a573bf8c2007293eac5ce0ea5daab1
Natalie Weizenbaum <nweiz@google.com>
2025-12-23 13:54:53 -0800

    Add typings for JSRecord and some unsafe extensions for JSObject (#487)
    * Add typings for JSRecord and some unsafe extensions for JSObject
    
    This doesn't include JSObject extensions that require types that
    aren't defined yet, like JSSymbolicRecord.
    
    * Document js_linterop/lib/unsafe.dart
    
    * Fix WASM test
    
    * Reformat
    
    * Fix lints
    
    * Code review

---
+++ js_interop/lib/src/unsafe/object.dart
@@ -0,0 +1,133 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:js_interop';
+
+@JS('Object.assign')
+external void _assign(
+  JSObject target, [
+  JSAny? source1,
+  JSAny? source2,
+  JSAny? source3,
+  JSAny? source4,
+]);
+
+@JS('Object.entries')
+external JSArray<JSArray<JSAny?>> _entries(JSObject object);
+
+@JS('Object.freeze')
+external void _freeze(JSObject object);
+
+@JS('Reflect.get')
+external JSAny? _get(JSObject object, JSAny name, JSAny? thisArg);
+
+@JS('Object.getOwnPropertyNames')
+external JSArray<JSString> _getOwnPropertyNames(JSObject object);
+
+@JS('Object.getOwnPropertySymbols')
+external JSArray<JSSymbol> _getOwnPropertySymbols(JSObject object);
+
+@JS('Object.hasOwn')
+external bool _hasOwn(JSObject object, JSAny property);
+
+@JS('Object.keys')
+external JSArray<JSString> _keys(JSObject object);
+
+@JS('Reflect.ownKeys')
+external JSArray<JSAny> _ownKeys(JSObject object);
+
+@JS('Reflect.set')
+external bool _set(JSObject object, JSAny name, JSAny? value, JSAny? thisArg);
+
+@JS('Object.values')
+external JSArray<JSAny?> _values(JSObject object);
+
+/// Additional instance methods for the `dart:js_interop` [JSObject] type meant
+/// to be used when the names of properties or methods are not known statically.
+extension JSObjectUnsafeExtension on JSObject {
+  /// See [`Object.entries()`].
+  ///
+  /// [`Object.entries()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+  List<(String, JSAny?)> get entries => [
+    for (var entry in _entries(this).toDart)
+      ((entry[0] as JSString).toDart, entry[1]),
+  ];
+
+  /// See [`Reflect.ownKeys()`].
+  ///
+  /// The return value contains only [JSString]s and [JSSymbol]s.
+  ///
+  /// [`Reflect.ownKeys()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/ownKeys
+  List<JSAny> get ownKeys => _ownKeys(this).toDart;
+
+  /// See [`Object.getOwnPropertyNames()`].
+  ///
+  /// [`Object.getOwnPropertyNames()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyNames
+  List<JSString> get ownPropertyNames => _getOwnPropertyNames(this).toDart;
+
+  /// See [`Object.getOwnPropertySymbols()`].
+  ///
+  /// [`Object.getOwnPropertySymbols()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertySymbols
+  List<JSSymbol> get ownPropertySymbols => _getOwnPropertySymbols(this).toDart;
+
+  /// See [`Object.keys()`].
+  ///
+  /// [`Object.keys()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  List<JSString> get keys => _keys(this).toDart;
+
+  /// See [`Object.values()`].
+  ///
+  /// [`Object.values()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+  List<JSAny?> get values => _values(this).toDart;
+
+  /// See [`Object.assign()`].
+  ///
+  /// [`Object.assign()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+  void assign([
+    JSObject? source1,
+    JSObject? source2,
+    JSObject? source3,
+    JSObject? source4,
+  ]) => _assign(this, source1, source2, source3, source4);
+
+  /// See [`Object.freeze()`].
+  ///
+  /// [`Object.freeze()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+  void freeze() => _freeze(this);
+
+  /// See [`Reflect.get()`].
+  ///
+  /// The [name] must be a [JSString] or a [JSSymbol].
+  ///
+  /// [`Reflect.get()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect/get
+  R getPropertyWithThis<R extends JSAny?>(JSAny name, JSAny? thisArg) =>
+      _get(this, name, thisArg) as R;
+
+  /// See [`Object.hasOwn()`].
+  ///
+  /// The [name] must be a [JSString] or a [JSSymbol].
+  ///
+  /// [`Object.hasOwn()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwn
+  bool hasOwnProperty(JSAny name) => _hasOwn(this, name);
+
+  /// See [`Reflect.set()`].
+  ///
+  /// The [name] must be a [JSString] or a [JSSymbol].
+  ///
+  /// [`Reflect.set()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect/set
+  bool setPropertyWithThis(JSAny name, JSAny? thisArg, JSAny? value) =>
+      _set(this, name, value, thisArg);
+
+  /// See [`Object.isPrototypeOf()`].
+  ///
+  /// [`Object.isPrototypeOf()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/isPrototypeOf
+  external bool isPrototypeOf(JSObject other);
+
+  /// See [`Object.propertyIsEnumerable()`].
+  ///
+  /// The [name] must be a [JSString] or a [JSSymbol].
+  ///
+  /// [`Object.propertyIsEnumerable()`]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/propertyIsEnumerable
+  external bool propertyIsEnumerable(JSAny name);
+}
---
+++ js_interop/test/record_test.dart
@@ -0,0 +1,262 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.p
+
+import 'dart:js_interop';
+
+import 'package:test/test.dart';
+
+import 'package:js_interop/js_interop.dart';
+
+void main() {
+  late JSRecord<JSNumber> record;
+  setUp(() => record = {"foo": 1.toJS, "bar": 2.toJS}.toJSRecord);
+
+  test(
+    "pairs",
+    () => expect(record.pairs, equals([("foo", 1.toJS), ("bar", 2.toJS)])),
+  );
+
+  test("entries", () {
+    var entries = record.entries.toList();
+    expect(entries.length, equals(2));
+    expect(entries[0].key, equals("foo"));
+    expect(entries[0].value, equals(1.toJS));
+    expect(entries[1].key, equals("bar"));
+    expect(entries[1].value, equals(2.toJS));
+  });
+
+  test("isEmpty", () {
+    expect(record.isEmpty, isFalse);
+    expect(JSRecord().isEmpty, isTrue);
+  });
+
+  test("isNotEmpty", () {
+    expect(record.isNotEmpty, isTrue);
+    expect(JSRecord().isNotEmpty, isFalse);
+  });
+
+  test("keys", () => expect(record.keys, equals(["foo", "bar"])));
+
+  test("length", () => expect(record.length, equals(2)));
+
+  test("values", () => expect(record.values, equals([1.toJS, 2.toJS])));
+
+  test(
+    "toDart",
+    () => expect(record.toDart, equals({"foo": 1.toJS, "bar": 2.toJS})),
+  );
+
+  test("JSRecord()", () => expect(JSRecord().toDart, equals({})));
+
+  test(
+    "JSRecord.ofRecord()",
+    () => expect(
+      JSRecord.ofRecord(record).toDart,
+      equals({"foo": 1.toJS, "bar": 2.toJS}),
+    ),
+  );
+
+  test(
+    "JSRecord.ofMap()",
+    () => expect(
+      JSRecord.ofMap({"foo": 1.toJS, "bar": 2.toJS}).toDart,
+      equals({"foo": 1.toJS, "bar": 2.toJS}),
+    ),
+  );
+
+  test(
+    "JSRecord.fromEntries()",
+    () => expect(
+      JSRecord.fromEntries([
+        MapEntry("foo", 1.toJS),
+        MapEntry("bar", 2.toJS),
+      ]).toDart,
+      equals({"foo": 1.toJS, "bar": 2.toJS}),
+    ),
+  );
+
+  test(
+    "JSRecord.fromPairs()",
+    () => expect(
+      JSRecord.fromPairs([("foo", 1.toJS), ("bar", 2.toJS)]).toDart,
+      equals({"foo": 1.toJS, "bar": 2.toJS}),
+    ),
+  );
+
+  test("addAll()", () {
+    record.addAll({"bar": 3.toJS, "baz": 4.toJS});
+    expect(
+      record.toDart,
+      equals({"foo": 1.toJS, "bar": 3.toJS, "baz": 4.toJS}),
+    );
+  });
+
+  test("addAllFromRecord()", () {
+    record.addAllFromRecord({"bar": 3.toJS, "baz": 4.toJS}.toJSRecord);
+    expect(
+      record.toDart,
+      equals({"foo": 1.toJS, "bar": 3.toJS, "baz": 4.toJS}),
+    );
+  });
+
+  test("addEntries()", () {
+    record.addEntries([MapEntry("bar", 3.toJS), MapEntry("baz", 4.toJS)]);
+    expect(
+      record.toDart,
+      equals({"foo": 1.toJS, "bar": 3.toJS, "baz": 4.toJS}),
+    );
+  });
+
+  test("addPairs()", () {
+    record.addPairs([("bar", 3.toJS), ("baz", 4.toJS)]);
+    expect(
+      record.toDart,
+      equals({"foo": 1.toJS, "bar": 3.toJS, "baz": 4.toJS}),
+    );
+  });
+
+  test("clear()", () {
+    record.clear();
+    expect(record.toDart, equals({}));
+  });
+
+  test("containsKey()", () {
+    expect(record.containsKey("foo"), isTrue);
+    expect(record.containsKey("baz"), isFalse);
+  });
+
+  test("containsValue()", () {
+    expect(record.containsValue(1.toJS), isTrue);
+    expect(record.containsValue(3.toJS), isFalse);
+  });
+
+  test("forEach()", () {
+    var args = <(String, JSNumber)>[];
+    record.forEach(
+      expectAsync2((key, value) {
+        args.add((key, value));
+      }, count: 2),
+    );
+    expect(args, equals([("foo", 1.toJS), ("bar", 2.toJS)]));
+  });
+
+  test(
+    "map()",
+    () => expect(
+      record.map((key, value) => MapEntry("_$key", (value.toDartInt + 1).toJS)),
+      equals({"_foo": 2.toJS, "_bar": 3.toJS}),
+    ),
+  );
+
+  group("putIfAbsent()", () {
+    test("present key", () {
+      expect(
+        record.putIfAbsent("foo", expectAsync0(() => 0.toJS, count: 0)),
+        equals(1.toJS),
+      );
+      expect(record.toDart, equals({"foo": 1.toJS, "bar": 2.toJS}));
+    });
+
+    test("absent key", () {
+      expect(
+        record.putIfAbsent("baz", expectAsync0(() => 0.toJS)),
+        equals(0.toJS),
+      );
+      expect(
+        record.toDart,
+        equals({"foo": 1.toJS, "bar": 2.toJS, "baz": 0.toJS}),
+      );
+    });
+  });
+
+  group("remove()", () {
+    test("present key", () {
+      expect(record.remove("foo"), equals(1.toJS));
+      expect(record.toDart, equals({"bar": 2.toJS}));
+    });
+
+    test("absent key", () {
+      expect(record.remove("baz"), isNull);
+      expect(record.toDart, equals({"foo": 1.toJS, "bar": 2.toJS}));
+    });
+  });
+
+  test("removeWhere()", () {
+    record.removeWhere((key, _) => key.startsWith("f"));
+    expect(record.toDart, equals({"bar": 2.toJS}));
+  });
+
+  group("update()", () {
+    group("no ifAbsent()", () {
+      test("present key", () {
+        expect(
+          record.update(
+            "foo",
+            expectAsync1((value) => (value.toDartInt + 1).toJS),
+          ),
+          equals(2.toJS),
+        );
+        expect(record.toDart, equals({"foo": 2.toJS, "bar": 2.toJS}));
+      });
+
+      test(
+        "absent key",
+        () => expect(
+          () => record.update("baz", expectAsync1((_) => 0.toJS, count: 0)),
+          throwsArgumentError,
+        ),
+      );
+    });
+
+    group("with ifAbsent()", () {
+      test("present key", () {
+        expect(
+          record.update(
+            "foo",
+            expectAsync1((value) => (value.toDartInt + 1).toJS),
+            ifAbsent: expectAsync0(() => 0.toJS, count: 0),
+          ),
+          equals(2.toJS),
+        );
+        expect(record.toDart, equals({"foo": 2.toJS, "bar": 2.toJS}));
+      });
+
+      test("absent key", () {
+        expect(
+          record.update(
+            "baz",
+            expectAsync1((_) => 0.toJS, count: 0),
+            ifAbsent: expectAsync0(() => 0.toJS),
+          ),
+          equals(0.toJS),
+        );
+        expect(
+          record.toDart,
+          equals({"foo": 1.toJS, "bar": 2.toJS, "baz": 0.toJS}),
+        );
+      });
+    });
+  });
+
+  test("updateAll()", () {
+    record.updateAll(
+      expectAsync2((_, value) => (value.toDartInt + 1).toJS, count: 2),
+    );
+    expect(record.toDart, equals({"foo": 2.toJS, "bar": 3.toJS}));
+  });
+
+  test("[]", () {
+    expect(record["foo"], equals(1.toJS));
+    expect(record["baz"], isNull);
+  });
+
+  test("[]=", () {
+    record["foo"] = 3.toJS;
+    record["baz"] = 4.toJS;
+    expect(
+      record.toDart,
+      equals({"foo": 3.toJS, "bar": 2.toJS, "baz": 4.toJS}),
+    );
+  });
+}

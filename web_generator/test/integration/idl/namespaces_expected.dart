3202d7c2545769f791649c90987888ee35c7145c
Harshita Yadav <seemayadavanuj123@gmail.com>
2026-01-29 00:09:04 +0530

    web_generator: emit WebIDL namespaces as static extension types

--- web_generator/test/integration/idl/namespaces_expected.dart
+++ web_generator/test/integration/idl/namespaces_expected.dart
@@ -10,18 +10,16 @@ import 'dart:js_interop';
 typedef Comparator = JSFunction;
 typedef LogLevel = String;
 extension type ConfigOptions._(JSObject _) implements JSObject {}
-@JS()
-external $MyLibrary get MyLibrary;
-@JS('MyLibrary')
-extension type $MyLibrary._(JSObject _) implements JSObject {
-  static const int VERSION_MAJOR = 1;
 
+@JS('MyLibrary')
+extension type MyLibrary(JSObject _) implements JSObject {
+  static const int VERSION_MAJOR = 1;
   static const int VERSION_MINOR = 4;
 
-  external void initialize();
-  external String stringify(JSObject input);
-  external JSPromise<JSAny?> fetchResource(String url);
-  external void log(String message, [LogLevel level]);
-  external ConfigOptions getDefaultConfig();
-  external void forEach(JSArray<JSString> items, Comparator compareFn);
+  static external void initialize();
+  static external String stringify(JSObject input);
+  static external JSPromise<JSAny?> fetchResource(String url);
+  static external void log(String message, [LogLevel level]);
+  static external ConfigOptions getDefaultConfig();
+  static external void forEach(JSArray<JSString> items, Comparator compareFn);
 }

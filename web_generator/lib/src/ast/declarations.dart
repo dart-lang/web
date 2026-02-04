5edd2ddf9a509607920667953a959c68294272bd
Kevin Moore <kevmoo@users.noreply.github.com>
2026-02-03 17:18:04 -0800

    Fix use_null_aware_elements lint (#518)

--- web_generator/lib/src/ast/declarations.dart
+++ web_generator/lib/src/ast/declarations.dart
@@ -711,7 +711,7 @@ class NamespaceDeclaration extends NestableDeclaration
             options: options,
             parent: this,
           );
-          methods.addAll([if (constr != null) constr]);
+          if (constr != null) methods.add(constr);
           break;
         default:
           break;
@@ -830,7 +830,7 @@ class CompositeDeclaration extends TypeDeclaration {
           propertyDeclarations.add(enumeration.cloneAsProperty());
         case final ClassDeclaration cl:
           final constr = _extractConstrFromClass(cl, parent: namespace);
-          methods.addAll([if (constr != null) constr]);
+          if (constr != null) methods.add(constr);
         default:
           break;
       }
@@ -936,7 +936,7 @@ class ClassDeclaration extends TypeDeclaration {
     return super._emit(
       options,
       abstract: abstract,
-      extendees: [if (extendedType case final extendee?) extendee],
+      extendees: [?extendedType],
       implementees: implementedTypes,
     );
   }

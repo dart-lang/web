9896fd9ca798fddf98ac87e25f8059a3ffd08f17
kevmoo <kevmoo@google.com>
2026-05-09 21:30:23 -0700

    fix: escape HTML-like tags inside doc comments to prevent unintended_html_in_doc_comment lint

---
+++ js_interop_gen/test/integration/interop_gen/comment_escaping_expected.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: non_constant_identifier_names
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:js_interop' as _i1;
+
+/// Documentation for EscapedComment.
+/// To use this properly, you must have &lt;language&gt; configuration files.
+/// Check `List<T>` and `<T>` value.
+///
+/// ```ts
+/// const value: List<T> = [];
+/// ```
+extension type EscapedComment._(_i1.JSObject _) implements _i1.JSObject {
+  external String value;
+}
---
+++ js_interop_gen/test/integration/interop_gen/comment_escaping_input.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Documentation for EscapedComment.
+ * To use this properly, you must have <language> configuration files.
+ * Check `List<T>` and `<T>` value.
+ *
+ * ```ts
+ * const value: List<T> = [];
+ * ```
+ */
+export interface EscapedComment {
+	value: string;
+}

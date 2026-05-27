fe77505ef65540c3b0fe90a246d4a1a8e1978169
kevmoo <kevmoo@google.com>
2026-05-27 11:40:09 -0700

    fix formatting

--- js_interop_gen/test/integration/interop_gen/_tuples.dart
+++ js_interop_gen/test/integration/interop_gen/_tuples.dart
@@ -6,7 +6,8 @@ import 'dart:js_interop' as _i1;
 
 extension type JSTuple2<A extends _i1.JSAny?, B extends _i1.JSAny?>._(
   _i1.JSArray<_i1.JSAny?> _
-) implements _i1.JSArray<_i1.JSAny?> {
+)
+    implements _i1.JSArray<_i1.JSAny?> {
   A get $1 => (_[0] as A);
 
   B get $2 => (_[1] as B);
